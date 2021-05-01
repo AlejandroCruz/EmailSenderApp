54cbb819628af4332ca61d684215e3619bb1531a
Alejandro Cruz <acdeveloper@outlook.com>
2021-05-01 17:35:16 -0400

    Set initial project configuration provider.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace EmailSenderApp
 {
@@ -6,7 +7,24 @@ namespace EmailSenderApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using IHost host = CreateHostBuilder(args).Build();
         }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, configuration) =>
+                {
+                    configuration.Sources.Clear();
+
+                    IHostEnvironment environment = hostingContext.HostingEnvironment;
+
+                    configuration
+                    .AddJsonFile("apsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"apsettings.{environment.EnvironmentName}.json");
+
+                    IConfigurationRoot configRoot = configuration.Build();
+
+
+                });
     }
 }
