477bfb71260de34b154c36d83a18a0cde3faee5c
Alejandro Cruz <acdeveloper@outlook.com>
2021-06-12 12:47:44 -0400

    Default builder for registering services through default plumbing; removed domain sub-dir (edit references); use of localdb.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
 ﻿using EmailSenderApp.DataInfrastructure;
 using EmailSenderApp.DataInfrastructure.Repositories;
-using EmailSenderApp.Domain.Data.Entities;
+using EmailSenderApp.Domain.DataEntities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace EmailSenderApp
@@ -17,63 +15,31 @@ namespace EmailSenderApp
     class Program
     {
         private static IConfiguration _configuration;
-        private static IServiceProvider _serviceProvider;
 
-        static async Task Main()
+        static async Task Main(string[] args)
         {
-            SetConfiguration();
-            SetLogger();
-            ConfigureServices();
+            IHostBuilder
+                hostBuilder = Host.CreateDefaultBuilder(args);
+            hostBuilder = AppConfiguration(hostBuilder);
+            IHost host = AppServices(hostBuilder);
 
-            await ApplicationProcess();
+            SetLogger();
+
+            await ApplicationProcess(host);
 
             //DisposeResources();
         }
 
-        private static void SetConfiguration()
-        {
-            _configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory()) // .exe dir
-                .AddJsonFile("AppConfig/appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"AppConfig/appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
-                .AddEnvironmentVariables()
-                .Build();
-        }
-
-        private static void SetLogger()
-        {
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(_configuration)
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .CreateLogger();
-        }
-
-        private static void ConfigureServices()
-        {
-            IServiceCollection services = new DefaultServiceProviderFactory()
-                .CreateBuilder(new ServiceCollection());
-
-            services.AddDbContext<OrderContext>((_, options) =>
-            {
-                options.UseSqlServer(_configuration.GetConnectionString("Default"));
-            });
-            services.AddScoped<OrderRepository>();
-
-            _serviceProvider = services.BuildServiceProvider();
-        }
-
-        private static async Task ApplicationProcess()
+        private static async Task ApplicationProcess(IHost host)
         {
             Log.Logger.Information("Getting orders.");
 
-            //string sql = $@"SELECT Id, Name FROM {_configuration["Database"]}.dbo.Names";
-            OrderRepository repository = _serviceProvider.GetRequiredService<OrderRepository>();
+            OrderRepository repository = host.Services.GetRequiredService<OrderRepository>();
             IEnumerable<Order> orders = await repository.GetOrdersAsync();
 
             if (orders != default)
             {
-                Console.WriteLine("Printing orders:"); 
+                Console.WriteLine("Printing orders:");
 
                 foreach (Order order in orders)
                 {
@@ -81,6 +47,43 @@ namespace EmailSenderApp
                 }
             }
 
+        }
+
+        private static IHostBuilder AppConfiguration(IHostBuilder hostBuilder)
+        {
+            return hostBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.Sources.Clear();
+
+                IHostEnvironment environment = hostingContext.HostingEnvironment;
+
+                _configuration = config.AddJsonFile("AppConfig/appsettings.json", optional: false, reloadOnChange: true)
+                   .AddJsonFile($"AppConfig/appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                   //.AddUserSecrets<Program>()
+                   .Build();
+            });
+        }
+
+        private static IHost AppServices(IHostBuilder hostBuilder)
+        {
+            hostBuilder.ConfigureServices(services =>
+            {
+                services.AddDbContext<OrderContext>(options =>
+                    options.UseSqlServer( _configuration.GetConnectionString("Default") ));
+                services.AddTransient<OrderRepository>();
+            });
+
+            return hostBuilder.Build();
+        }
+
+        private static void SetLogger()
+        {
+            // TODO - ACruz: How to print log levels (info/warning/error)
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(_configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
         }
 
         private static void DisposeResources()
